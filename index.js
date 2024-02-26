4a640ea7fee2041e6fca2d14bff4deaa92dd8dd8
fermino135 <132684644+fermino135@users.noreply.github.com>
2024-02-26 11:41:02 -0300

    Update java.js

--- index.js
+++ index.js
@@ -1,7 +1,54 @@
 var pessoa = "832.192.304-34"
 
-console.log(`Conteúdo inicial da variavel: ${pessoa} | Tipo:${typeof(pessoa)}`);
+console.log(`Conteúdo inicial da variavel: ${pessoa} | Tipo: ${typeof(pessoa)}`);
 
 pessoa = 259;
 
-console.log(`Mudei o conteúdo: ${pessoa} | Tipo:${typeof(pessoa)}`);
+console.log(`Mudei o conteúdo: ${pessoa} | Tipo: ${typeof(pessoa)}`);
+
+var num1 = 10
+var num2 = 20
+
+var soma = 0
+
+console.log(`Total é: ${soma}`);
+
+soma = num1 + num2;
+console.log(`Total da soma ${num1}+${num2}=${soma}`);
+
+//subtração
+
+var sub = 0
+
+console.log(`Total é: ${sub}`);
+
+sub = num1 - num2;
+
+console.log(`Total da subtação ${num1}-${num2}=${sub}`);
+
+// multiplicação
+
+var mul = 0
+
+console.log(`Total é: ${mul}`);
+
+mul = num1 * num2;
+
+console.log(`Total da multiplicação ${num1}*${num2}=${mul}`);
+
+// divisão
+
+var div = 0
+
+console.log(`Total é: ${div}`);
+
+div = num1 / num2;
+
+console.log(`Total da divisão ${num1}/${num2}=${div}`);
+
+
+
+
+
+
+
